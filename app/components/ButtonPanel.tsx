d0c2783aa13d71da958fe3087ac5e92edc67fe4b
carsonjc14 <42079975+carsonjc14@users.noreply.github.com>
2025-02-05 23:15:44 -0500

    props error
    fixed props error in ButtonPanel

--- app/components/ButtonPanel.tsx
+++ app/components/ButtonPanel.tsx
@@ -1,16 +1,23 @@
 
+interface buttonStyles{
+    changeStyle(color:string): void;
+    homeLight: string;
+    redLight: string;
+    orangeLight: string;
+    yellowLight: string;
+    blueLight: string;
+}
 
-
-const ButtonPanel = (props) => {
+const ButtonPanel = ({changeStyle, homeLight, yellowLight, orangeLight, redLight, blueLight}:buttonStyles) => {
 
     return (
 
         <div className="ml-[5vw] flex flex-col gap-4 items-center my-auto row-start-1 col-start-4 mx-auto aspecct-[2/9] w-[16vw] ">
-            <div onClick={() => props.changeStyle("Home")} className={props.homeLight} ><button className="tv-button">HOME</button></div>
-            <div onClick={() => props.changeStyle("Red")} className={props.redLight} ><button className="tv-button">ABOUT</button></div>
-            <div onClick={() => props.changeStyle("Orange")} className={props.orangeLight} ><button className="tv-button">CLUBS</button></div>
-            <div onClick={() => props.changeStyle("Yellow")} className={props.yellowLight} ><button className="tv-button text-[.7em]">PROJECTS</button></div>
-            <div onClick={() => props.changeStyle("Blue")} className={props.blueLight} ><button className="tv-button text-[.75em] ">CONTACT</button></div>
+            <div onClick={() => changeStyle("Home")} className={homeLight} ><button className="tv-button">HOME</button></div>
+            <div onClick={() => changeStyle("Red")} className={redLight} ><button className="tv-button">ABOUT</button></div>
+            <div onClick={() => changeStyle("Orange")} className={orangeLight} ><button className="tv-button">CLUBS</button></div>
+            <div onClick={() => changeStyle("Yellow")} className={yellowLight} ><button className="tv-button text-[.7em]">PROJECTS</button></div>
+            <div onClick={() => changeStyle("Blue")} className={blueLight} ><button className="tv-button text-[.75em] ">CONTACT</button></div>
         </div>
 
 
